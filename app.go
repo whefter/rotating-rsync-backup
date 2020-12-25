eb23c90085c26b4c1fd11788f700b84180a6fc26
whefter <wh@elehq.de>
2020-12-25 10:00:52 +0000

    ci: tag branch images with v3, fix scheduled tags

--- app.go
+++ app.go
@@ -31,7 +31,7 @@ func main() {
 
 	app := &cli.App{
 		Name:    "rotating-rsync-backup",
-		Version: "v3.0.0-beta.4",
+		Version: "v3.0.0-beta.5",
 		Usage:   "Create hardlinked backups using rsync and rotate them",
 		Flags: []cli.Flag{
 			&cli.StringFlag{
