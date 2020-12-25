3eb0037a1a2c34f406fbf08797f6b4f47adab7e2
whefter <wh@elehq.de>
2020-12-25 09:55:03 +0000

    chore(version): bump version to v3.0.0-beta.4

--- app.go
+++ app.go
@@ -31,7 +31,7 @@ func main() {
 
 	app := &cli.App{
 		Name:    "rotating-rsync-backup",
-		Version: "v3.0.0-beta.3",
+		Version: "v3.0.0-beta.4",
 		Usage:   "Create hardlinked backups using rsync and rotate them",
 		Flags: []cli.Flag{
 			&cli.StringFlag{
